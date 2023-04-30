974a0d13abeed189e09b2941dc0a127cb2dd7af1
Euna Kim <keunaa88@gmail.com>
2023-04-30 21:38:37 +1000

    test

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,12 @@
 
+
 const { REACT_APP_SERVER } = process.env;
 
-const API_DEV = `${REACT_APP_SERVER}/api`;
-const API_PRODUCT = '';
-const baseURL = process.env.NODE_ENV === 'development' ? API_DEV : API_PRODUCT;
-console.log('baseURL', baseURL)
+// const API_DEV = `${REACT_APP_SERVER}/api`;
+// const API_PRODUCT = `${REACT_APP_SERVER}/api`;
+const baseURL = `${REACT_APP_SERVER}/api`; //process.env.NODE_ENV === 'development' ? API_DEV : API_PRODUCT;
+console.log('baseURL??', baseURL)
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 
 const api = {
   baseURL,
