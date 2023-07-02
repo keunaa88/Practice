ae39c16bea1ce320ce9949ad79968cfd76bcb7d3
Euna Kim <keunaa88@gmail.com>
2023-07-02 21:45:33 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from 'components/Footer';
 // const Detail = lazy(() => import('./routes/Detail'));
 // const Cart = lazy(() => import('./routes/Cart'));
 
-//bugs 1.0
+//bugs 1.0 - main
 function App() {
 
   return (
